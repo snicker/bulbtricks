97b68564ecf52edcd99aaa6053c9b81bc249bae9
snicker <ngordon779@gmail.com>
2019-09-16 21:57:50 -0700

    make transitionbulb self contained

--- bulbs/transitionbulb.py
+++ bulbs/transitionbulb.py
@@ -1,17 +1,29 @@
-from bulbs.rampupbulb import RampUpBulb
+from bulbs.bulb import Bulb
 
-class TransitionBulb(RampUpBulb):
+class TransitionBulb(Bulb):
     def __init__(self, frombulb = None, tobulb = None, delay = 3):
-        RampUpBulb.__init__(self, minbrightness=0, maxbrightness=1, delay=delay)
+        Bulb.__init__(self)
         self._frombulb = frombulb
         self._tobulb = tobulb
+        self._delay = delay
         
     @property
-    def minbrightness(self):
+    def delay(self):
+        return self._delay
+        
+    @property
+    def startingbrightness(self):
         return self._frombulb.brightness if self._frombulb else 0
         
     @property
-    def maxbrightness(self):
+    def endingbrightness(self):
         return self._tobulb.brightness if self._tobulb else 1
+        
+    def step(self, direction):
+        if self.startingbrightness > self.endingbrightness:
+            direction *= -1
+        self.brightness += self.speed * direction * abs(self.startingbrightness - self.endingbrightness) / (self.delay * self.frequency / self.ticks_per_step)
+        if (self.startingbrightness >= self.endingbrightness and self.brightness <= self.endingbrightness) or (self.endingbrightness >= self.startingbrightness and self.brightness >= self.endingbrightness):
+            self.brightness = self.endingbrightness
         
         
