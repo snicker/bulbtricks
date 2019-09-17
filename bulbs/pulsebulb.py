583b29ad4719f0a778447b69968df133032154f5
snicker <ngordon779@gmail.com>
2019-09-16 21:36:36 -0700

    revert pulsebulb all the way

--- bulbs/pulsebulb.py
+++ bulbs/pulsebulb.py
@@ -1,16 +1,27 @@
-from bulbs.rampupbulb import RampUpBulb
+from bulbs.bulb import Bulb
 
-class PulseBulb(RampUpBulb):
+class PulseBulb(Bulb):
     def __init__(self, minbrightness = 0, maxbrightness = 1, delay = 3):
-        RampUpBulb.__init__(self, minbrightness=minbrightness, maxbrightness=maxbrightness, delay=delay)
+        Bulb.__init__(self)
+        self._delay = delay
+        self._minbrightness = minbrightness
+        self._maxbrightness = maxbrightness
         self._pulse_direction = 1
         
     @property
-    def pulsedirection(self):
-        return self._pulse_direction
+    def minbrightness(self):
+        return self._minbrightness
+
+    @property
+    def maxbrightness(self):
+        return self._maxbrightness
+        
+    @property
+    def delay(self):
+        return self._delay
         
     def step(self, direction):
-        self.brightness += self.speed * direction * (self.maxbrightness - self.minbrightness) / (self.delay * self.frequency / self.ticks_per_step)
+        self.brightness += self.speed * direction * self._pulse_direction * (self.maxbrightness - self.minbrightness) / (self.delay * self.frequency / self.ticks_per_step)
         if self.brightness >= self.maxbrightness or self.brightness <= self.minbrightness:
             self.brightness = max(self.minbrightness,min(self.maxbrightness,self.brightness))
             self._pulse_direction *= -1
