b50a0c53bf4906aef704106b60072e4785f86b0e
snicker <ngordon779@gmail.com>
2019-09-16 20:26:43 -0700

    add min and max brightness to rampupbulb and inherit rampupbulb in pulsebulb

--- bulbs/pulsebulb.py
+++ bulbs/pulsebulb.py
@@ -1,28 +1,17 @@
-from bulbs.bulb import Bulb
+from bulbs.rampupbulb import RampUpBulb
 
-class PulseBulb(Bulb):
+class PulseBulb(RampUpBulb):
     def __init__(self, minbrightness = 0, maxbrightness = 1, delay = 3):
-        Bulb.__init__(self)
-        self._delay = delay
-        self._minbrightness = minbrightness
-        self._maxbrightness = maxbrightness
+        RampUpBulb.__init__(self, minbrightness=minbrightness, maxbrightness=maxbrightness, delay=delay)
         self._pulse_direction = 1
         
     @property
-    def minbrightness(self):
-        return self._minbrightness
-
-    @property
-    def maxbrightness(self):
-        return self._maxbrightness
-        
-    @property
-    def delay(self):
-        return self._delay
+    def pulsedirection(self):
+        return self._pulse_direction
         
     def step(self, direction):
-        self.brightness += self.speed * direction * self._pulse_direction * (self.maxbrightness - self.minbrightness) / (self.delay * self.frequency / self.ticks_per_step)
+        RampUpBulb.step(self, direction)
+        self.brightness *= self.pulsedirection
         if self.brightness >= self.maxbrightness or self.brightness <= self.minbrightness:
-            self.brightness = max(self.minbrightness,min(self.maxbrightness,self.brightness))
             self._pulse_direction *= -1
         
--- bulbs/rampupbulb.py
+++ bulbs/rampupbulb.py
@@ -1,13 +1,25 @@
 from bulbs.bulb import Bulb
 
 class RampUpBulb(Bulb):
-    def __init__(self, delay = 3):
+    def __init__(self, minbrightness = 0, maxbrightness = 1, delay = 3):
         Bulb.__init__(self)
         self._delay = delay
+        self._minbrightness = minbrightness
+        self._maxbrightness = maxbrightness
+        
+    @property
+    def minbrightness(self):
+        return self._minbrightness
+
+    @property
+    def maxbrightness(self):
+        return self._maxbrightness
         
     @property
     def delay(self):
         return self._delay
         
     def step(self, direction):
-        self.brightness += self.speed * direction * 1.0 / (self.delay * self.frequency / self.ticks_per_step)
+        self.brightness += self.speed * direction * (self.maxbrightness - self.minbrightness) / (self.delay * self.frequency / self.ticks_per_step)
+        if self.brightness >= self.maxbrightness or self.brightness <= self.minbrightness:
+            self.brightness = max(self.minbrightness,min(self.maxbrightness,self.brightness))
