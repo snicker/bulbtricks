7f3bdc050a46a6350c5e394ce579d22cbad8f50f
snicker <ngordon779@gmail.com>
2019-09-19 22:18:22 -0700

    try one more time with HighlightEffect

--- effects/highlighteffect.py
+++ effects/highlighteffect.py
@@ -25,6 +25,8 @@ class HighlightEffect_Up(Effect):
     def __init__(self, options):
         self.options = options
         self.transitionbulb = None
+        self.filtered = []
+        self.focusbulb = None
         
     @property
     def completed(self):
@@ -50,21 +52,22 @@ class HighlightEffect_Up(Effect):
     
     def initialize(self, matrix):
         self._matrix = matrix
-        self._original_matrix = copy.deepcopy(self._matrix._matrix)
-        filtered = []
-        focusbulb = self._matrix.at(self.options.column, self.options.row)
+        self.filtered = []
+        self.focusbulb = self._matrix.at(self.options.column, self.options.row)
         for column in range(self._matrix.columns):
             for row in range(self._matrix.rows):
                 bulb = self._matrix.at(column, row)
-                if bulb not in filtered:
+                if bulb not in self.filtered:
                     fbulb = self.get_fbulb(bulb)
-                    filtered.append(fbulb)
+                    self.filtered.append(fbulb)
                     self._matrix.replace(bulb, fbulb)
         self.transitionbulb = self.get_tbulb(focusbulb)
         self._matrix.add(self.transitionbulb, self.options.column, self.options.row)
                     
     def remove(self):
-        self._matrix._matrix = self._original_matrix
+        for fbulb in self.filtered:
+            self._matrix.replace(fbulb, fbulb.original)
+        self._matrix.replace(self.transitionbulb, self.focusbulb)
 
 class HighlightEffect_Down(HighlightEffect_Up):
     def on_completed(self):
