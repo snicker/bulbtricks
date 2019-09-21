029551ef13385e0e27ac44e3b851bd751851e212
snicker <ngordon779@gmail.com>
2019-09-21 14:07:14 -0700

    add BlinkCycleEffect looping through all blinks

--- effects/effect.py
+++ effects/effect.py
@@ -1,6 +1,8 @@
 class Effect:
     def __init__(self):
         self._matrix = None
+        self._ticks_per_step = 1
+        self._ticks = 0
         
     def initialize(self, matrix):
         self._matrix = matrix
@@ -8,7 +10,18 @@ class Effect:
     def remove(self):
         pass
         
+    def next_step_in(self, seconds):
+        if self._matrix:
+            self._ticks = 0
+            self._ticks_per_step = int(seconds * self._matrix.frequency)
+        
     def tick(self):
+        self._ticks += 1
+        if self._ticks >= self.ticks_per_step:
+            self._ticks = 0
+            self.step()
+        
+    def step(self):
         pass
         
     @property
